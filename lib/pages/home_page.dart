259856b20ae141b07e1ed37ad6949af123bcde94
HeyAksh <aksh07.me@gmail.com>
2025-06-22 18:35:37 +0530

    Added Drawer

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -37,6 +37,121 @@ class _HomePageState extends State<HomePage> {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.grey[300],
+
+      //due to just pushed on intropage we will by default
+      // get a back icon we will reomove this to add hamburger icon to open drawer
+      appBar: AppBar(
+        backgroundColor: Colors.transparent,
+        leading: Builder(
+          builder: (context) {
+            return IconButton(
+              onPressed:
+                  () =>
+                      Scaffold.of(
+                        context,
+                      ).openDrawer(),
+              icon: Icon(
+                Icons.menu,
+                color: Colors.black,
+              ),
+            );
+          },
+        ),
+      ),
+
+      drawer: Drawer(
+        backgroundColor: Colors.grey[900],
+        child: Column(
+          mainAxisAlignment:
+              MainAxisAlignment.spaceBetween,
+          children: [
+            //logo
+            Column(
+              children: [
+                DrawerHeader(
+                  child: Center(
+                    child: Image.asset(
+                      'lib/images/nike.png',
+                      color: Colors.white,
+                      height: 80,
+                      width: 160,
+                      fit: BoxFit.contain,
+                      // ),
+                    ),
+                  ),
+                ),
+
+                //divider (to separate contents)
+                Padding(
+                  padding:
+                      const EdgeInsets.symmetric(
+                        vertical: 25.0,
+                      ),
+                  child: Divider(
+                    color: Colors.white,
+                  ),
+                ),
+
+                const Padding(
+                  padding: EdgeInsets.only(
+                    left: 25.0,
+                  ),
+                  child: ListTile(
+                    leading: Icon(
+                      Icons.home,
+                      color: Colors.white,
+                    ),
+                    title: Text(
+                      'Home',
+                      style: TextStyle(
+                        color: Colors.white,
+                      ),
+                    ),
+                  ),
+                ),
+
+                const Padding(
+                  padding: EdgeInsets.only(
+                    left: 25.0,
+                  ),
+                  child: ListTile(
+                    leading: Icon(
+                      Icons.info,
+                      color: Colors.white,
+                    ),
+                    title: Text(
+                      'About',
+                      style: TextStyle(
+                        color: Colors.white,
+                      ),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+
+            const Padding(
+              padding: EdgeInsets.only(
+                left: 25.0,
+                bottom: 25.0,
+              ),
+              child: ListTile(
+                leading: Icon(
+                  Icons.logout,
+                  color: Colors.white,
+                ),
+                title: Text(
+                  'Logout',
+                  style: TextStyle(
+                    color: Colors.white,
+                  ),
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+
       bottomNavigationBar: MyBottomNavBar(
         onTabChange:
             (index) => navigateBottomBar(index),
