1a32aba34c4c01a80becd0cb7f0923520fe8e584
HeyAksh <aksh07.me@gmail.com>
2025-06-23 11:43:59 +0530

    Created Shoe Tile and respective shoe model

---
+++ lib/components/shoe_tile.dart
@@ -0,0 +1,100 @@
+import 'package:flutter/material.dart';
+
+import '../models/shoe.dart';
+
+class ShoeTile extends StatelessWidget {
+  final Shoe shoe;
+  const ShoeTile({super.key, required this.shoe});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: EdgeInsets.only(left: 25),
+      width: 280,
+      decoration: BoxDecoration(
+        color: Colors.grey[100],
+        borderRadius: BorderRadius.circular(12),
+      ),
+      child: Column(
+        mainAxisAlignment:
+            MainAxisAlignment.spaceBetween,
+        children: [
+          //Shoe pic
+          ClipRRect(
+            borderRadius: BorderRadius.circular(
+              12,
+            ),
+            child: Image.asset(shoe.imagePath),
+          ),
+
+          //Description
+          Text(
+            shoe.description,
+            style: TextStyle(color: Colors.grey),
+          ),
+
+          //price + details
+          Padding(
+            padding: const EdgeInsets.only(
+              left: 25.0,
+            ),
+            child: Row(
+              mainAxisAlignment:
+                  MainAxisAlignment.spaceBetween,
+              crossAxisAlignment:
+                  CrossAxisAlignment.start,
+              children: [
+                Column(
+                  crossAxisAlignment:
+                      CrossAxisAlignment.start,
+                  children: [
+                    //shoe name
+                    Text(
+                      shoe.name,
+                      style: TextStyle(
+                        fontWeight:
+                            FontWeight.bold,
+                        fontSize: 20,
+                      ),
+                    ),
+
+                    const SizedBox(height: 5),
+
+                    //price
+                    Text(
+                      '\$${shoe.price}',
+                      style: TextStyle(
+                        color: Colors.grey,
+                      ),
+                    ),
+                  ],
+                ),
+
+                //plus button
+                Container(
+                  padding: const EdgeInsets.all(
+                    20,
+                  ),
+                  decoration: const BoxDecoration(
+                    color: Colors.black,
+                    borderRadius:
+                        BorderRadius.only(
+                          topLeft:
+                              Radius.circular(12),
+                          bottomRight:
+                              Radius.circular(12),
+                        ),
+                  ),
+                  child: const Icon(
+                    Icons.add,
+                    color: Colors.white,
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
