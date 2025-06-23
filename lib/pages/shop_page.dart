c9eb071c1f953e8ab4ad673fc981b791e8ffe92d
HeyAksh <aksh07.me@gmail.com>
2025-06-23 11:46:46 +0530

    Displayed Tiles on Home Page

--- lib/pages/shop_page.dart
+++ lib/pages/shop_page.dart
@@ -1,4 +1,7 @@
+import 'package:e_commerce_app/components/shoe_tile.dart';
 import 'package:flutter/material.dart';
+
+import '../models/shoe.dart';
 
 class ShopPage extends StatelessWidget {
   const ShopPage({super.key});
@@ -78,6 +81,36 @@ class ShopPage extends StatelessWidget {
               ),
             ],
           ),
+        ),
+
+        const SizedBox(height: 10),
+
+        Expanded(
+          child: ListView.builder(
+            padding: EdgeInsets.only(right: 25),
+            scrollDirection: Axis.horizontal,
+            itemCount: 10,
+            itemBuilder: (context, index) {
+              Shoe shoe = Shoe(
+                name: 'Air Jordan',
+                price: '2500',
+                imagePath:
+                    'lib/images/JORDAN+MVP.png',
+                description: "abc",
+              );
+
+              return ShoeTile(shoe: shoe);
+            },
+          ),
+        ),
+
+        const Padding(
+          padding: EdgeInsets.only(
+            top: 25.0,
+            left: 25.0,
+            right: 25.0,
+          ),
+          child: Divider(color: Colors.white),
         ),
       ],
     );
